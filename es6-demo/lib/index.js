53cee71d2cd6d14ee6e802bd10d175b352598c5a
wangpin <pin.wang@hp.com>
2016-01-29 14:52:09 +0800

    Add es6 demo

---
+++ es6-demo/lib/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+require('core-js/shim');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = (function () {
+  function Person(name) {
+    _classCallCheck(this, Person);
+
+    this.name = name;
+  }
+
+  _createClass(Person, [{
+    key: 'sayHello',
+    value: function sayHello() {
+      return 'Hello ' + this.name + '!';
+    }
+  }, {
+    key: 'sayHelloThreeTimes',
+    value: function sayHelloThreeTimes() {
+      var hello = this.sayHello();
+      return (hello + ' ').repeat(3);
+    }
+  }]);
+
+  return Person;
+})();
+
+exports.default = Person;
---
+++ es6-demo/src/app.js
@@ -0,0 +1,18 @@
+export default class Employee{
+
+  constructor( name,age ) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getName(){
+    return this.name;
+  }
+
+  getAge(){
+    return this.age;
+  }
+
+
+
+}
